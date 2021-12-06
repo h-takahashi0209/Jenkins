e0ecb62891880e42959be4a47eaf27266049c534
h-takahashi <93642791+h-takahashi0209@users.noreply.github.com>
2021-12-07 00:24:04 +0900

    Add files via upload

---
+++ Unity/Bat/BuildApplication.bat
@@ -0,0 +1,9 @@
+"C:\ProgramFiles\Unity\Hub\Editor\2021.2.0f1\Editor\Unity.exe" ^
+-batchmode ^
+-quit ^
+-logfile ^
+.\build.log ^
+-projectPath ^
+D:\Project\Private\Unity\%1\%1 ^
+-executeMethod ^
+TakahashiH.Build.ApplicationBuild.%2
---
+++ Unity/Bat/UploadAppToDeployGate.bat
@@ -0,0 +1,5 @@
+curl ^
+-H "Authorization: token %1%" ^
+-F "file=@D:\Project\Private\Unity\%2\%2\Output\Android\%2.%3" ^
+-F "message=sample" ^
+"https://deploygate.com/api/users/h-takahashi0209/apps"
